59ef7b950d6748eb87e2d03bf07a0098fcadcd61
Shivam Kashyap <shivamkumarkashyap12@gmail.com>
2023-04-02 17:45:20 +0530

    Addingup new end point i.e.. DeleteNote

--- routes/notes.js
+++ routes/notes.js
@@ -66,9 +66,35 @@ router.put('/updatenote/:id', fetchuser, async (req, res) => {
         note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
         res.json({ note });
 
-    } catch (error) {
-
+    } catch (err) {
+        // catching unknown erros
+        console.error(err);
+        res.status(500).send("Internal Server Error")
     }
-})
+});
+
+// ROUT 4: Delete an existing note using DELETE: /api/notes/deleteNote/:id. Login require
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+        // Finding the note to be updated and update it.
+        let note = await Notes.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not found");
+        }
+
+        // allow deletion only if user owns this Note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Notes.findByIdAndDelete(req.params.id);
+        res.json({ sucess: "Sucessfully deleted note" });
+
+    } catch (err) {
+        // catching unknown erros
+        console.error(err);
+        res.status(500).send("Internal Server Error")
+    }
+});
 
 module.exports = router
